69f769d2bee89da4fb5d4122ad0730988e7c78ce
EmanuelCampos <rebortgames@gmail.com>
2019-09-27 22:04:29 -0300

    adicionando usuários

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -40,24 +40,28 @@ class Home extends Component {
     }
     
     loadProfile = async () => {
+        const profiles = this.state.profiles;
         const response = await api.get(`${this.title.value}`)
-        this.setState({ profiles: response.data});
+        profiles.push(response.data)
+        this.setState({ profiles: profiles });
     }
 
   render() {
     const { profiles } = this.state;
+    console.log('array:', profiles)
     return (
       <div className="container">
         <div className="box">
           <div className="box-itens">
             <img src={Logo}></img>
-            <form>
+            <form autoComplete="off">
               <input
                 ref={c => (this.title = c)}
                 name="title"
                 id="username"
                 type="text"
                 placeholder="Usuário do Github"
+                autoComplete="false"
               ></input>
             </form>
             <button
@@ -76,34 +80,35 @@ class Home extends Component {
             }
           <div className="users">
             
-
-          <Card className="e-card">
-      <div className="details">
-        
-        <CardContent className="content">
-          <Typography component="h5" variant="h5">
-            {profiles.name}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-          {profiles.blog}
-          </Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-          </Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-          <Link style={{ textDecoration: 'none', color: '#A2A7AC', fontstyle: 'italic' }} to="google.com">Repositorios :</Link> {profiles.public_repos}  
-          </Typography>
-          <Typography variant="subtitle2" color="textSecondary">
-          <Link style={{ textDecoration: 'none', color: '#A2A7AC', fontstyle: 'italic' }} to="google.com">Seguidores :</Link> {profiles.followers}      
-          </Typography>
-        </CardContent>
-      </div>
-      <CardMedia
-        className="cover"
-        image={profiles.avatar_url}
-        title="Live from space album cover"
-      />
- 
-    </Card>
+          {profiles.map(profile => 
+                    <Card className="e-card">
+                <div className="details">
+                  
+                  <CardContent className="content">
+                    <Typography component="h5" variant="h5">
+                      {profile.name}
+                    </Typography>
+                    <Typography variant="subtitle1" color="textSecondary">
+                    {profile.blog}
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary">
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary">
+                    <Link style={{ textDecoration: 'none', color: '#A2A7AC', fontstyle: 'italic' }} to="google.com">Repositorios :</Link> {profile.public_repos}  
+                    </Typography>
+                    <Typography variant="subtitle2" color="textSecondary">
+                    <Link style={{ textDecoration: 'none', color: '#A2A7AC', fontstyle: 'italic' }} to="google.com">Seguidores :</Link> {profile.followers}      
+                    </Typography>
+                  </CardContent>
+                </div>
+                <CardMedia
+                  className="cover"
+                  image={profile.avatar_url}
+                  title="Live from space album cover"
+                />
+          
+              </Card>
+          )}
           </div>
         </div>
       </div>
